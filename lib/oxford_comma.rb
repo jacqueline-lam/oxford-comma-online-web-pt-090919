c52fbba1c3943117a228fc6075cd1fb432af1d11
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-17 21:41:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 def oxford_comma(array)
-  # array.join(", ")
+  # array.join(", ") to convert array to string 
   if array.length == 1
     string = ""
     array.each { |element| string += element}
   elsif array.length == 2
     array.join(" and ")  
   elsif array >= 3 
-    array[-1] = " and " + array[-1]
-    
+    array[-1] = "and " + array[-1]
+    array.join(", ")
 end
 
 
