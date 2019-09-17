98329d98858bea14b6c652506e4e6b9192963161
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-17 21:44:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   # array.join(", ") to convert array to string 
   if array.length == 1
     string = ""
-    array.each { |element| string += element}
+    string += array[0] 
   elsif array.length == 2
     array.join(" and ")  
   elsif array >= 3 
